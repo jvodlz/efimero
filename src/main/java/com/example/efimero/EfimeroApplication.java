5fbbb2eaa72700bd7da3e54f36e08c9337b08b40
JT <95458994+jvodlz@users.noreply.github.com>
2025-10-07 23:31:08 +1300

    initial commit

---
+++ src/main/java/com/example/efimero/EfimeroApplication.java
@@ -0,0 +1,13 @@
+package com.example.efimero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfimeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfimeroApplication.class, args);
+	}
+
+}
